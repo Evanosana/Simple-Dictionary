a9a189a0dfd2c9901fc4ac40dc4b4ea23f19ec25
Evanosana <evanfennern44@gmail.com>
2024-10-16 09:44:16 -0700

    done

--- main.js
+++ main.js
@@ -2,28 +2,35 @@ const searchButton = document.getElementById('searchButton');
 const container = document.getElementById('container');
 
 let searchInput = document.getElementById('search');
+let noCapsInput = ''
 let quickCheck = false
 
 searchButton.addEventListener('click', () => {
-    console.log(searchInput.value);
-    searchData(searchInput.value);
+    commitSearch();
 });
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
         searchInput.focus();
-        searchData(searchInput.value);
+        commitSearch();
     }
 });
 
+function commitSearch(){
+    noCapsOnInput();
+    searchData(noCapsInput);
+}
+function noCapsOnInput(){
+    noCapsInput = searchInput.value.toLowerCase();
+}
 function searchData(data) {
-    if (!searchInput) {
+    if (!searchInput.value) {
         if(quickCheck){
             deleteData();
         }
-        displayData(searchInput.value, true)
+        displayData(data, true)
         return;
     } if(!(data in dictionaryData)){
-        displayData(searchInput.value, true)
+        displayData(data, true)
     }
     if(quickCheck) {
         deleteData();
@@ -35,24 +42,20 @@ function deleteData() {
         return
     }
     const definitionDeleter = event.target.parentNode.parentNode.parentNode.children[2].children[0];
-    console.log(definitionDeleter);
     definitionDeleter.remove();
 }
 function displayData(data, error) {
     if(error){
+        const errorText = document.createElement('p');
+        container.appendChild(errorText);
+        errorText.classList.add('defs1');
+
         if(!searchInput.value){
-            console.log(true);
-            const errorText = document.createElement('p');
-            container.appendChild(errorText);
             errorText.innerText = 'Please enter a word';
-            errorText.classList.add('defs1');
             quickCheck = true;
             return;
         } else{
-            const errorText = document.createElement('p');
             errorText.innerText = `'${searchInput.value}' was not found in the dictionary`;
-            container.appendChild(errorText);
-            errorText.classList.add('defs1');
             quickCheck = true;
             return;
         }
@@ -72,7 +75,6 @@ function displayData(data, error) {
     partOfSpeech.classList.add('defs');
     definitionOne.classList.add('defs');
 
-    
     if ('two' in dictionaryData[data].definitions) {
         const definitionTwo = document.createElement('p');
         definitionTwo.innerText = '2). ' + dictionaryData[data].definitions.two;
@@ -81,8 +83,6 @@ function displayData(data, error) {
     }
     quickCheck = true
 }
-
-// Data for the dictionary
 const dictionaryData = {
     yes: {
         partOfSpeech: 'noun',
@@ -148,7 +148,133 @@ const dictionaryData = {
         definitions: {
             one: 'A sudden and ignominious failure; a fiasco.'
         }
-    }
+    },
+    element: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'Represents the building blocks of a web page.',
+            two: 'Represents the value of each index in arrays.'
+        }
+    },
+    array: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A variable that holds multiple values. Each index is called an element.'
+        }
+    },
+    string: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A piece of text encased inside of \'singlequotes\', "doublequotes", or `backticks`.'
+        }
+    },
+    index: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'The ith element or character.'
+        }
+    },
+    loop: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A process that repeats indefinitely until its condition is reached.'
+        }
+    },
+    tag: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'Tags in HTML are elements enclosed in <angle brackets> used to structure content.',
+            two: 'Typically tags have an <opening> and </closing> tag, but some exceptions occur.'
+        }
+    },
+    attribute: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'Provides additional information to modify an HTML element like IDs or Classes.',
+            two: 'Can be placed in the opening tag.'
+        }
+    },
+    selector: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'In CSS, selectors signify what elements the styles will apply towards.'
+        }
+    },
+    property: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'Used in CSS to define what style to apply on an element.'
+        }
+    },
+    function: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'In JavaScript, a function is a reusable block of code that can be called to run.',
+            two: 'Can be defined using the function keyword and can use parameters to help with certain behaviors.'
+        }
+    },
+    variable: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A variable in JavaScript is a container to hold data values.',
+            two: 'Can be created using var, let, or const and can hold data types like strings, numbers, or objects.'
+        }
+    },
+    event: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'An event is an action that occurs in the browser, such as a mouse click, which can be detected through JavaScript.'
+        }
+    },
+    class: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A class is an attribute that can be applied to an element to tell CSS to apply specific styles to it.'
+        }
+    },
+    anchor: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'Uses <a> to insert a hyperlink to an internal or external page.',
+            two: 'It also uses the href attribute to specify the path/url.'
+        }
+    },
+    input: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'An interactive field that lets the user enter data.'
+        }
+    },
+    image: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'An image is a picture displayed on the screen.',
+            two: 'Uses the <img> tag to embed itself to the HTML page and uses src attribute to specify the path/url.'
+        }
+    },
+    grid: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A structure made up of columns and rows.'
+        }
+    },
+    alert: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'The alert() function sends a popup message to the user with an OK button.'
+        }
+    },
+    transition: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'A way of telling the computer how to make something appear or change instead of it being instantaneous.'
+        }
+    },
+    debug: {
+        partOfSpeech: 'noun',
+        definitions: {
+            one: 'The process of finding and fixing errors in code, typically using the console to find out information.'
+        }
+    }    
 };
-// searchData('yes');
 searchInput.focus();
